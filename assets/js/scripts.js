6e05c3e40abcecf9b346dbb0e1830ffdecec1303
guilhermealvesdev <guilherme.alves.trash@gmail.com>
2021-04-25 02:50:37 -0300

    Início do header.

---
+++ assets/js/scripts.js
@@ -0,0 +1,7 @@
+﻿const botaoMenu = document.querySelector("[data-botao-menu]")
+const caixaMenuLinks = document.querySelector("[data-menu-links]");
+
+botaoMenu.addEventListener('click', (e) => {
+    e.preventDefault();
+    caixaMenuLinks.classList.toggle("ativo");
+})
